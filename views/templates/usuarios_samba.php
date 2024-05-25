f27b2df2fe44a55f9ea3094a544b03928458443b
JARO-Hub <juliancitobz13@gmail.com>
2024-05-25 12:00:15 -0400

    Update usuarios_samba.php

--- views/templates/usuarios_samba.php
+++ views/templates/usuarios_samba.php
@@ -224,12 +224,14 @@
 
                                                             <!--end::Export-->
                                                             <!--begin::Add user-->
-                                                            <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                                                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-kt-user-table-toolbar="base"
                                                                 data-bs-target="#kt_modal_add_user">
                                                                 <i class="ki-duotone ki-plus fs-2"></i>Añadir usuario Samba</button>
-                                                            <button type="button" class="btn btn-danger" data-bs-toggle="modal"
-                                                                data-bs-target="#EliminarUsuarioSamba">
-                                                                <i class="ki-duotone ki-plus fs-2"></i>Eliminar usuario Samba</button>
+                                                            <div class="d-flex justify-content-end align-items-center d-none" data-kt-user-table-toolbar="selected">
+                                                                <div class="fw-bold me-5">
+                                                                    <span class="me-2" data-kt-user-table-select="selected_count"></span>Selected</div>
+                                                                <button type="button" class="btn btn-danger" data-kt-user-table-select="delete_selected">Delete Selected</button>
+                                                            </div>
                                                             <!--end::Add user-->
                                                         </div>
                                                         <!--end::Toolbar-->
