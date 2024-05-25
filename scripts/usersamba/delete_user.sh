4bb5f940cf4763a52b52a8000f39af0cbdd37352
JARO-Hub <juliancitobz13@gmail.com>
2024-05-25 18:20:54 -0400

    Update delete_user.sh

--- scripts/usersamba/delete_user.sh
+++ scripts/usersamba/delete_user.sh
@@ -5,26 +5,24 @@ sambausername=$2
 
 # Función para manejar errores
 handle_error() {
-    echo "Error: $1"
+    echo "Error: $1" >&2
     exit 1
 }
 
 # Verificar que el nombre de usuario no esté vacío
-if [ ! -z "$sambausername" ]; then
-    # Eliminar el usuario de Samba
-    echo "$passw" | sudo -S smbpasswd -x "$sambausername" || handle_error "No se pudo eliminar el usuario de Samba"
-    echo "$passw" | sudo -S pdbedit -x -u "$sambausername" || handle_error "No se pudo eliminar el usuario de Samba"
-
-    # Eliminar el usuario del sistema
-    echo "$passw" | sudo -S userdel "$sambausername" || handle_error "No se pudo eliminar el usuario del sistema"
-
-    # Eliminar el directorio home del usuario
-    echo "$passw" | sudo -S rm -rf /home/"$sambausername" || handle_error "No se pudo eliminar el directorio home del usuario"
-
-    # Reiniciar el servicio de Samba
-    echo "$passw" | sudo -S systemctl restart smb || handle_error "No se pudo reiniciar el servicio de Samba"
-    echo "$passw" | sudo -S systemctl restart nmb || handle_error "No se pudo reiniciar el servicio de NetBIOS"
+if [ -z "$sambausername" ]; then
+    handle_error "El nombre de usuario no puede estar vacío"
 fi
+
+# Eliminar el usuario de Samba
+echo "$passw" | sudo -S smbpasswd -x "$sambausername" || handle_error "No se pudo eliminar el usuario de Samba"
+
+# Eliminar el usuario del sistema
+echo "$passw" | sudo -S userdel -r "$sambausername" || handle_error "No se pudo eliminar el usuario del sistema"
+
+# Reiniciar el servicio de Samba para aplicar cambios
+echo "$passw" | sudo -S systemctl restart smb || handle_error "No se pudo reiniciar el servicio de Samba"
+echo "$passw" | sudo -S systemctl restart nmb || handle_error "No se pudo reiniciar el servicio de NetBIOS"
 
 echo "Usuario eliminado correctamente"
 exit 0
