3c045841ce00992a5518d8b6241f15f22f9cc96d
JARO-Hub <juliancitobz13@gmail.com>
2024-05-25 20:27:43 -0400

    Update update_user.sh

--- scripts/usersamba/update_user.sh
+++ scripts/usersamba/update_user.sh
@@ -3,7 +3,7 @@
 passw=$1
 new_sambapassword=$2
 new_username=$3
-username=$4  # Username actual para operaciones de actualización
+username=$4  # Nombre de usuario actual para operaciones de actualización
 
 # Función para manejar errores
 handle_error() {
@@ -16,21 +16,22 @@ if [ -z "$username" ]; then
     handle_error "El nombre de usuario actual no puede estar vacío"
 fi
 
-# Verificar si el usuario existe en el sistema
-if ! getent passwd "$username" > /dev/null 2>&1; then
-    handle_error "El usuario $username no existe en el sistema"
+# Verificar si el usuario existe en Samba
+if ! sudo -S pdbedit -L -u "$username" > /dev/null 2>&1; then
+    handle_error "El usuario $username no existe en Samba"
 fi
 
-# Renombrar usuario del sistema si se proporciona un nuevo nombre de usuario
+# Renombrar usuario en Samba y en el sistema si se proporciona un nuevo nombre de usuario
 if [ ! -z "$new_username" ]; then
     echo "Renombrando el usuario $username a $new_username..."
     echo "$passw" | sudo -S usermod -l "$new_username" "$username" || handle_error "No se pudo renombrar el usuario del sistema"
+    echo "$passw" | sudo -S pdbedit -r -u "$username" -n "$new_username" || handle_error "No se pudo renombrar el usuario en Samba"
     username=$new_username
 fi
 
 # Cambiar la contraseña del usuario de Samba
 if [ ! -z "$new_sambapassword" ]; then
-    echo "$passw" | sudo echo "Cambiando la contraseña del usuario $username en Samba..."
+    echo "Cambiando la contraseña del usuario $username en Samba..."
     echo -e "$new_sambapassword\n$new_sambapassword" | sudo -S smbpasswd -s "$username" || handle_error "No se pudo cambiar la contraseña del usuario de Samba"
 fi
 
