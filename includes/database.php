a9346e04da760dc7f4e84abbbd6a8c51ae3105f4
apazaJose <130260924+apazaJose@users.noreply.github.com>
2024-11-19 16:33:55 -0400

    ..

--- includes/database.php
+++ includes/database.php
@@ -27,7 +27,7 @@ function getCurrentPid($db): int
 
 // Conectarnos a la base de datos
 try {
-    $db = connectDB('localhost', '5433', 'taller_bd', 'postgres', 'anabel');
+    $db = connectDB('localhost', '5432', 'taller_bd', 'phpadmin', '123456');
     /** @var int $pid */
     $pid = getCurrentPid($db);
     // La conexión fue exitosa
